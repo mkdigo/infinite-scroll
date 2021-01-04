1400fd1dc111f75ec8484e78e49ce5ef647ac19e
Rodrigo Mukudai <mkdigo@gmail.com>
2021-01-04 09:30:11 +0900

    fix router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './styles/globalStyle.css'
 
 import Header from './components/Header'
-import Routes from './Routes';
+import Routes from './Routes'
 import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="pokemon">
       <Header />
       <Routes />
       <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
